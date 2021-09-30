a66bbe69a18c39826478bf843b6bcc7f16fdf293
gparada <gparada@carltonone.com>
2021-09-30 10:52:04 -0700

    cleaned up files, added composition api

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<div class="app">
+  <p> {{jobs[0].location}}</p>
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Job from './types/Job'
+import { defineComponent, reactive, toRefs, ref } from 'vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  //Composition API
+  setup() {
+    //every job that is loaded here must follow the interface 'Job' structure
+    const jobs = ref<Job[]>([
+      {title: 'Farm worker', location: 'lon lon ranch', salary: 30000, id: '1'},
+      {title: 'Cleaner', location: 'Toronto', salary: 40000, id: '2'},
+      {title: 'Gardener', location: 'Strawberry Fields', salary: 38000, id: '3'},
+      {title: 'Painter', location: 'Hamilton', salary: 30000, id: '4'},
+      {title: 'Dancer', location: 'School of dance', salary: 50000, id: '5'}
+    ])
+    return { jobs }
+  },
+  //Options API
+  // data() {
+  //   return {
+  //     name: 'Link',
+  //     //type casting
+  //     age: 25 as number | string
+  //   }
+  // },
+  methods: {
+
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
