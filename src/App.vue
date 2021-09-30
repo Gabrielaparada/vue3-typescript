a6f90dfea9284923ac0829ff94319835c546f25a
gparada <gparada@carltonone.com>
2021-09-30 12:04:05 -0700

    displaying data
    Created type and interface, pass data using props and set up prop validation using PropTypes

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,78 @@
 <template>
-<div class="app">
-  <p> {{jobs[0].location}}</p>
-</div>
+	<div class="app">
+    <button @click="handleClick('title')"> Order by title</button>
+    <button @click="handleClick('salary')"> Order by salary </button>
+    <button @click="handleClick('location')"> Order by location</button>
+		<JobList :jobs="jobs" :order="order" />
+	</div>
 </template>
 
 <script lang="ts">
-import Job from './types/Job'
-import { defineComponent, reactive, toRefs, ref } from 'vue';
-
+import Job from "./types/Job";
+import { defineComponent, reactive, toRefs, ref } from "vue";
+import JobList from "./components/JobList.vue";
+import OrderTerm from './types/OrderTerm'
 export default defineComponent({
-  name: 'App',
-  components: {
-    
-  },
-  //Composition API
-  setup() {
-    //every job that is loaded here must follow the interface 'Job' structure
-    const jobs = ref<Job[]>([
-      {title: 'Farm worker', location: 'lon lon ranch', salary: 30000, id: '1'},
-      {title: 'Cleaner', location: 'Toronto', salary: 40000, id: '2'},
-      {title: 'Gardener', location: 'Strawberry Fields', salary: 38000, id: '3'},
-      {title: 'Painter', location: 'Hamilton', salary: 30000, id: '4'},
-      {title: 'Dancer', location: 'School of dance', salary: 50000, id: '5'}
-    ])
-    return { jobs }
-  },
-  //Options API
-  // data() {
-  //   return {
-  //     name: 'Link',
-  //     //type casting
-  //     age: 25 as number | string
-  //   }
-  // },
-  methods: {
-
-  }
+	name: "App",
+	components: {
+		JobList,
+	},
+	//Composition API
+	setup() {
+		//every job that is loaded here must follow the interface 'Job' structure
+		const jobs = ref<Job[]>([
+			{
+				title: "Farm worker",
+				location: "lon lon ranch",
+				salary: 30000,
+				id: "1",
+			},
+			{ title: "Cleaner", location: "Toronto", salary: 40000, id: "2" },
+			{
+				title: "Gardener",
+				location: "Strawberry Fields",
+				salary: 38000,
+				id: "3",
+			},
+			{ title: "Painter", location: "Hamilton", salary: 30000, id: "4" },
+			{ title: "Dancer", location: "School of dance", salary: 50000, id: "5" },
+		]);
+    // we are creating local state using a ref to keep track of the OrderTerm, initially it will be 'title'
+    //but it can change to be 'location' or 'salary' as stated in OrderTerm
+    const order = ref<OrderTerm>('title')
+    //passing OrderTerm as an argument to make sure we only accept the specific info we need title, location or salary.
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+		return { jobs, handleClick, order };
+	},
+	//Options API
+	// data() {
+	//   return {
+	//     name: 'Link',
+	//     //type casting
+	//     age: 25 as number | string
+	//   }
+	// },
+	methods: {},
 });
 </script>
 
 <style>
-
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
 </style>
--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,17 +1,71 @@
 <template>
-    
+	<div class="job-list">
+    <p>Ordered by {{order}}</p>
+		<ul>
+			<li :key="job.id" v-for="job in jobs">
+				<h2>{{ job.title }} - {{ job.location }}</h2>
+				<div class="salary">
+					<p>{{ job.salary }} rupees</p>
+				</div>
+				<div class="description">
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+						commodi illum optio aliquid, neque ut laborum amet nisi vel
+						sapiente?
+					</p>
+				</div>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from "vue";
+import Job from "@/types/Job";
+import OrderTerm from "@/types/OrderTerm";
 
 export default defineComponent({
-    // setup() {
-        
-    // },
-})
+	props: {
+		jobs: {
+			required: true,
+			type: Array as PropType<Job[]>,
+		},
+    order: {
+      require: true,
+      type: String as PropType<OrderTerm>
+    }
+	}
+});
 </script>
 
 <style scoped>
-
+  .job-list {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+  .job-list ul {
+    padding: 0;
+  }
+  .job-list li {
+    list-style-type: none;
+    background: white;
+    padding: 16px;
+    margin: 16px 0;
+    border-radius: 4px;
+  }
+  .job-list h2 {
+    margin: 0 0 10px;
+    text-transform: capitalize;
+  }
+  .salary {
+    display: flex;
+  }
+  .salary img {
+    width: 30px;
+  }
+  .salary p {
+    color: #17bf66;
+    font-weight: bold;
+    margin: 10px 4px;
+  }
 </style>
